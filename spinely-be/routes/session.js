2ed5f4b7aaa4db8fa44a12d8c4f82e69343570f5
trinkles <cmc0234@dlsud.edu.ph>
2024-02-17 20:20:49 +0800

    Reverted session.js routes 20240217

--- spinely-be/routes/session.js
+++ spinely-be/routes/session.js
@@ -6,13 +6,6 @@ const sessionRouter = express.Router();
 
 sessionRouter.post('/add', controller.addSession);
 sessionRouter.get('/get', controller.getUserSessions);
-sessionRouter.get('/elapsedTime', controller.getElapsedTime); // time passed during the session (in s)
-sessionRouter.get('/countProperPosture', controller.getProperOverall); // A proper out of N total postures
-sessionRouter.get('/forwardUpperSensor', controller.forwardUpperSensor); // B forward bends out of N total
-sessionRouter.get('/forwardMidSensor', controller.forwardMidSensor); // C forward bends out of N total
-sessionRouter.get('/forwardLowerSensor', controller.forwardLowerSensor); // D forward bends out of N total
-sessionRouter.get('/leftBentSensor', controller.leftBentSensor); // E left bends out of N total
-sessionRouter.get('/rightBentSensor', controller.forwardUpperSensor); // F right bends out of N total
 sessionRouter.put('/update', controller.updateSession); 
 
 
