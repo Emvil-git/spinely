a1c01d5757ec526e5add7d1934f0041b7ba8f987
Eduard Naldoza <naldozaeduardpd@gmail.com>
2024-02-23 18:51:04 +0300

    docs(sqlite): example syntax for sqlite3

--- spinely-be/controllers/users2.js
+++ spinely-be/controllers/users2.js
@@ -3,32 +3,28 @@ import bcrypt from "bcrypt";
 import { createAccessToken, decode } from '../auth.js';
 import sqlite from 'sqlite3';
 
-const db = new sqlite.Database("./spinely.db");
+// connect to the db like this. (the directory is relative to the root of the project. not this js file)
+const db = new sqlite.Database("./controllers/spinely.db");
 
 const createTable = async (req, res) => {
+    // Define the query
     console.log('createTable is running');
-    const query = `INSERT INTO asd(id, name) VALUES(1, john)`;
+    const query = `INSERT INTO asd(id, name) VALUES(1, 'john')`;
 
+    // Use db.run to run the INSERT Function. 
+    //  (use 'db.get' to get one row, and db.all for multiple rows)
     db.run(query, function(err) {
         if (err) { return console.log(err.message); }
         console.log(`A row has been inserted with rowid ${this.lastID}`);
     });
-    db.close();
-    res.status(200).json({'succ':'succ'})
 
+    // Close the connection for this function (not sure if entirely necessary)
+    db.close();
+
+    // return the json. Check the example I sent in messenger for GETs
+    res.status(200).json({'succ':'succ'});
 }
 
-// const signUp = async (req, res) => {
-//     logging.info(NAMESPACE, 'User sign up');
 
-//     let { name, username, email, password } = req.body;
-//     let query = `INSERT INTO users (name, username, email, password) VALUES ("${name}", "${username}", "${email}", "${ bcrypt.hashSync(password, 5)}")`;
-
-//     console.log(req.body);
-//     console.log(query);
-
-// }
-
-
-
+// export the function
 export default { createTable };
